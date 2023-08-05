146c65804fa30981501ed4fc2cd9ae8e0ba79a2c
Merkle Groot <merklegroot@gmail.com>
2023-08-04 22:15:47 -0400

    adding a release pipeline

---
+++ src/PrimSharp/Lid.cs
@@ -0,0 +1,14 @@
+﻿namespace PrimSharp;
+
+public record Lid : Prim, ISizedPrim
+{
+    public decimal Width { get; }
+    public decimal Breadth { get; }
+    public decimal Height { get; }
+    
+    public override string ToOpenScad()
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
